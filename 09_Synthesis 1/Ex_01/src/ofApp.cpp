00b64475be0a17448f41bbb5d74ddb200baadcdb
Komat <komatsu_takayuki@komat.local>
2016-07-28 02:51:46 +0900

    09_Synthesis 1

---
+++ 09_Synthesis 1/Ex_01/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetWindowShape(600, 600);
+    ofEnableSmoothing();
+    ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    int width = ofGetWidth();
+    int height = ofGetHeight();
+
+    ofFill();
+    ofSetColor(102);
+    ofLine(0, height/2, width, height/2);
+    ofNoFill();
+    ofFill();
+    ofSetColor(255, 204);
+    int d = mouseY / 2 + 10;
+    ofEllipse(mouseX, height / 2, d, d);
+    ofFill();
+    ofSetColor(255, 204);
+    int iX = width - mouseX;
+    int iY = height - mouseY;
+    int iD = (iY / 2) + 10;
+    ofEllipse(iX, height/2, iD, iD);}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ 09_Synthesis 1/Ex_02/ofApp.cpp
@@ -0,0 +1,97 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    ofSetWindowShape(600, 600);
+    ofEnableSmoothing();
+    ofHideCursor();
+    ofBackground(102);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+    ofNoFill();
+    ofFill();
+    ofSetColor(0);
+    ofRect(150, 150, 300, 300);
+
+    ofFill();
+    ofSetColor(255);
+
+    if ((mouseX > 150) && (mouseX < 450) &&
+            (mouseY > 150) && (mouseY < 450)) {
+        ofLine(0, 0, 150, 150);
+        ofLine(600, 0, 450, 150);
+        ofLine(450, 450, 600, 600);
+        ofLine(0, 600, 150, 450);
+    } else {
+        ofLine(150, 150, 450, 450);
+        ofLine(150, 450, 450, 150);
+    }
+
+    ofNoFill();
+    ofFill();
+    ofSetColor(0);
+    ofEllipse(mouseX, mouseY, 12, 12);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
